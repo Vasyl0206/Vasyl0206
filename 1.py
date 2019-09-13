9deff25fea7aada08b47801b5e98fa056d933990
Vasyl0206 <diachok87@gmail.com>
2019-09-13 15:03:07 +0300

    Homework1

---
+++ 1.py
@@ -0,0 +1,11 @@
+x = float (input ('a= '))
+y = float (input ('b= '))
+z = x+y
+print ('a+b= ' + str (z))
+c = x-y
+print ('a-b= ' + str (c))
+v = x*y
+print ('a*b= ' + str (v))
+n = x/y
+print ('a/b= ' + str (n))
+
---
+++ 5.py
@@ -0,0 +1,5 @@
+a = int (input ('Введіть число №1: '))
+b = int (input ('Введіть число №2: '))
+print (a,b)
+a,b = b,a
+print (a,b) 
