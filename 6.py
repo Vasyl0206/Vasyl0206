dc82a2dc799e162ac991492f19d43de6498ede66
Vasyl0206 <diachok87@gmail.com>
2019-09-18 15:34:16 +0300

    Add homework6

---
+++ 6.py
@@ -0,0 +1,78 @@
+##Ex1
+# i = 100
+# while i > 0:
+#     print (i)
+#     i = i-2
+
+# for i in range (1,101,2):
+#     print (i)
+
+# for i in range (1,100):
+#     if i % 2==0 :
+#         continue
+#     print (i)
+    
+# print (list(range(0,100,2)))    
+
+##Ex2  
+# i = 100
+# while i > 0: 
+#     i = i-1
+#     if i % 2 != 0:
+#         print (i)
+    
+
+# #Ex3
+# list_num = [2,4,6,7]
+# for item in list_num:
+#     if item %2 == 0:
+#         continue
+#         if item %2 != 0:    
+#             break
+#     print (item)
+
+
+# #Ex4
+# list_num = [2,4,6]
+# for a in range(len(list_num)):
+#     a = float(list_num[a])
+#     print (a) 
+
+# #Ex5
+# num = int(input ('Введіть число: '))
+# a = 0
+# b = 1
+# while b < num:
+#     print (a)
+#     c = a + b
+#     b = a
+#     a = c
+#     if a > num:
+#         break
+
+# #Ex6
+# list_str = ['str1','str2','str3','str4']
+# for a in list_str:
+#     print (a)
+
+# #Ex7
+# list_str = ['str1','str2','str3','str4']
+# for a in list_str:
+    
+#     for i in a:  
+#         print (i,end='# ')   
+ 
+# #Ex8
+# num = int (input ('Введіть число: ')) 
+# if num == 1 or num == 2 or num == 3 or num == 5 or num == 7:
+#     print ("Просте число")
+# elif num / num == 1 and num % 2== 0 or num / num == 1 and num % 3== 0 or num / num == 1 and num % 5== 0 or num / num == 1 and num % 7== 0 or num / num == 1 and num**0.5 % 2 ==0:
+#     print ('Складне число')
+# else :
+#     print ('Просте число')
+
+
+
+
+
+
