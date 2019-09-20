bc4cc484f824899debdd6d42d79cfb99d7eb5ce7
Vasyl0206 <diachok87@gmail.com>
2019-09-20 13:13:23 +0300

    add new work 7

---
+++ 7.py
@@ -0,0 +1,46 @@
+# #EX1
+# nums = []
+# k=int(input("Please enter the count of the elements of sequence: "))
+# for i in range(k):
+#      n = int(input("Please enter the element: "))
+#      nums.append(n)
+# print(nums)
+# max = nums[0]
+# min = nums[0]
+# for i in range(k):
+#     if nums[i] > max:
+#         max = nums[i]
+#     if nums[i] < min:
+#         min = nums[i]
+# # print("Maximum number = %d. Minimum number = %d." %(max, min))
+# print("Maximum number = {}. Minimum number = {}.".format(max, min))
+
+##EX2
+# k = [x for x in range (11) if x %2 == 0]
+# print (k)
+
+# a = [x for x in range (11) if x % 3 == 0]
+# print  (a)
+
+# b = [x for x in range (11) if x % 3 != 0 and x % 2 != 0]
+# print  (b)
+
+
+# #EX3
+# k = int(input("Number: "))
+# a = 1
+# for i in range(1,k+1):
+#     a *= i
+# print(a)
+    
+# #EX4
+# k = input ("Введіть логін ")
+# a = 'First'
+# while a != k:
+#     print ('Не вірний логін') 
+#     k = input ("Введіть логін ")  
+# else:
+#     print ("Вітаємо ", a)    
+
+
+
