c7cb7da6ab267611448bebfe393422b1aac33103
Vasyl0206 <diachok87@gmail.com>
2019-10-09 16:56:50 +0300

    Add homework 10

--- 10.py
+++ 10.py
@@ -2,23 +2,6 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("My first game")
-
-x=225
-y=225
-r=50
-vol=20
-done = True
-clock = pygame.time.Clock()
-
-def left(x,r,vol):
-    
-    x += vol
-    return right(x,r,vol)
-def right(x,r,vol):
-    
-    x -= vol
-    return left(x,r,vol)
-
 while done:
     pygame.time.delay(100)
     for event in pygame.event.get(): 
